0b55524680f432d6f60eff6b29660dec74ef5d92
asus_linux <sasha.sidenko09@gmail.com>
2026-03-04 09:55:23 +0300

    completed loops

---
+++ src/basics/loops/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // for loop
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"For loop iteration: {i}");
+        }
+        Console.WriteLine();
+
+        // while loop
+        int a = 0;
+        while (a <= 5)
+        {
+            Console.WriteLine($"While loop iteration: {a}");
+            a++;
+        }
+        Console.WriteLine();
+
+        // foreach loop
+        int[] nums = { 1, 2, 3, 4, 5 };
+        foreach (int num in nums)
+        {
+            Console.WriteLine($"Foreach loop num: {num}");
+        }
+
+
+    }
+}
