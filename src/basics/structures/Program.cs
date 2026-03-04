12b22c7fccbf73680fff92f1f3b771eca3750605
arch_pc <sasha.sidenko09@gmail.com>
2026-03-04 17:34:50 +0300

    completed structures

---
+++ src/basics/structures/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    // enum
+    enum Day
+    {
+        Monday,
+        Tuesday
+    }
+
+    // struct (хранится в стеке)
+    struct Point
+    {
+        public int X;
+        public int Y;
+        public override readonly string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+
+    // record
+    record Person(string Name, int Age);
+
+    static void Main()
+    {
+        Day today = Day.Monday;
+        Console.WriteLine(today == Day.Monday ? "Monday" : "Another day");
+
+        Point p;
+        p.X = 52;
+        p.Y = 42;
+        Console.WriteLine(p.ToString());
+
+        var p1 = new Person("Skibidi", 52);
+        var p2 = new Person("Skibidi", 52);
+        Console.WriteLine(p1 == p2); // у class был бы falses
+    }
+}
