eb38dfe469341633eb3371148fc16ce3f7721c31
asus_linux <sasha.sidenko09@gmail.com>
2026-03-04 16:18:01 +0300

    added methods

---
+++ src/basics/methods/Program.cs
@@ -0,0 +1,53 @@
+﻿class Program
+{
+    // Простой метод
+    static void printDa()
+    {
+        Console.WriteLine("da");
+    }
+
+    // Разные параметры
+    static void changeNum(ref int input, in int increment, out int result)
+    {
+        // ref - ссылка, можно изменять значение
+        // in - ссылка, только для чтения
+        // out - выходное значение
+        result = input + increment;
+    }
+
+    // Множество параметров
+    static int sumNums(params int[] nums)
+    {
+        int result = 0;
+
+        foreach (int num in nums)
+        {
+            result += num;
+        }
+
+        return result;
+    }
+
+    // Параметры по умолчанию
+    static void printHuman(in string name = "Stas", in int age = 52)
+    {
+        Console.WriteLine($"Name: {name}\nAge: {age}");
+    }
+
+    static void Main()
+    {
+        printDa();
+
+        int inputNum = 5;
+        changeNum(input: ref inputNum, increment: 3, result: out int a);
+        Console.WriteLine($"changeNum result: {a}");
+
+        int sumResult = sumNums(1, 2, 3, 4, 5, 6);
+        Console.WriteLine($"Sum result: {sumResult}");
+
+        int[] numArr = { 2, 4, 6, 8 };
+        Console.WriteLine($"Sum with arr result: {sumNums(numArr)}");
+
+        printHuman();
+    }
+}
